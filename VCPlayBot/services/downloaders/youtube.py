e4467135f4127e749f9fcb55646e253bca3f70b9
khayal-0508 <80122272+khayal-0508@users.noreply.github.com>
2021-08-30 17:40:57 +0400

    Update youtube.py

--- VCPlayBot/services/downloaders/youtube.py
+++ VCPlayBot/services/downloaders/youtube.py
@@ -23,14 +23,14 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
-            f"the provided video is {duration} minute(s)",
+            f"🛑 {DURATION_LIMIT} dəqiqədən böyük faylların yüklənməsinə icazə verilmir, "
+            f"bu fayl {duration} dəqiqədir",
         )
     try:
         ydl.download([url])
     except:
         raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
-            f"the provided video is {duration} minute(s)",
+            f"🛑 {DURATION_LIMIT} dəqiqədən böyük faylların yüklənməsinə icazə verilmir, "
+            f"bu fayl {duration} dəqiqədir",
         )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
