16a75239193885a3a546ef24ac650380cff926f6
khayal-0508 <80122272+khayal-0508@users.noreply.github.com>
2021-08-30 18:52:20 +0400

    Update ytsearch.py

--- VCPlayBot/modules/ytsearch.py
+++ VCPlayBot/modules/ytsearch.py
@@ -29,10 +29,10 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
+            text += f"Başlıq - {results[i]['title']}\n"
+            text += f"Uzunluq - {results[i]['duration']}\n"
+            text += f"Baxış - {results[i]['views']}\n"
+            text += f"Kanal - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
